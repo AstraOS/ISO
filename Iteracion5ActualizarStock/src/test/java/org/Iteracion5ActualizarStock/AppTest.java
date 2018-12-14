2026e843a99c8a9e2fcbde7c88ae402740f6cf61
Adrián <adrian.incertis@alu.uclm.es>
2018-12-14 13:02:54 +0100

    Update Test

--- Iteracion5ActualizarStock/src/test/java/org/Iteracion5ActualizarStock/AppTest.java
+++ Iteracion5ActualizarStock/src/test/java/org/Iteracion5ActualizarStock/AppTest.java
@@ -1,8 +1,9 @@
 package org.Iteracion5ActualizarStock;
 
-import junit.framework.Test;
+import org.junit.Test;
+
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
 
 /**
  * Unit test for simple App.
@@ -12,11 +13,11 @@ public class AppTest extends TestCase{
 	Ingrediente ing = new Ingrediente(1, 1, "Cebolla");
 	GestorCocina gc = new GestorCocina(ing.getUnidades());
 	
-	
+	@Test
 	public void testActualizarIngredientesCorrecto() {	
 		assertEquals(true, gc.actualizarIngredientes(ing));
 	}
-	
+	@Test
 	public void testComprobarUnidadesCorrecto() {
 		assertEquals(1, gc.getNIngredientes());
 	}
